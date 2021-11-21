428fd5e549086e76ad7c33025eb79ca51aded477
Nantakud <francesco.vacca.au@gmail.com>
2021-11-21 11:48:47 +0800

    contacts section

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,5 +1,27 @@
 import "./contacts.scss";
+import { useState } from "react";
 
 export default function Contacts() {
-  return <div className="contact" id="contact"></div>;
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
+
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="email" />
+          <textarea placeholder="message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thanks for your message. I'll reply shortly</span>}
+        </form>
+      </div>
+    </div>
+  );
 }
